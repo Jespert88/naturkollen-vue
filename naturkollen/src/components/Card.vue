6f5f633b16cfb8c08c8c54cb84d2d3a4b8aef622
Jesper <jesper_truedsson@hotmail.com>
2019-11-30 18:11:45 +0100

    trying to make the src tag pick up the url from json object

--- naturkollen/src/components/Card.vue
+++ naturkollen/src/components/Card.vue
@@ -1,21 +1,50 @@
 <template>
-  <div>
+<div>
 
     <div class="card" style="width: 18rem; box-shadow: 1px 1px 10px #222;">
-        <img src="https://loremflickr.com/400/400" class="card-img-top" alt="...">
+        <!-- <img v-bind:src="tree.image" v-bind:alt="tree.image" className='img-fluid' /> -->
+        <img v-bind:src="tree.image" v-bind:alt="tree.image" class="img-fluid" />
+             
         <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+            <h5 class="card-title"> {{tree.name}}</h5>
+            <button class="btn btn-primary" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample" v-on:click="toggle">
+                Button with data-target
+            </button>
+
+            <div class="collapse" id="collapseExample">
+                <div class="card card-body">
+                    <p class="card-text">
+                        <!-- {{user.email}} -->
+                        {{tree.description}}
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 
-  </div>
+</div>
 </template>
 
 <script>
 export default {
+    name: "Card",
+    props: {
+        user: {},
+        tree: {}
+    },
 
+    created: function () {
+       console.log(this.tree.img)
+    },
+
+    methods: {
+
+        toggle: function () {
+            let bodyID = this.tree.id;
+            console.log(bodyID);
+        }
+
+    }
 }
 </script>
 
--- naturkollen/src/containers/Trees.vue
+++ naturkollen/src/containers/Trees.vue
@@ -6,12 +6,10 @@
             <div class="col-sm-1 col-md-1 col-xl-0"></div>
             <div class="col-sm-12 col-md-10 col-xl-12">
                 <div id="grid">
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
-                    <Card />
+
+                    <!-- <Card v-for="user in userArray" :key="user.id" :user="user" /> -->
+                    <Card v-for="tree in treeArray" :key="tree.id" :tree="tree" />
+
                 </div>
             </div>
             <div class="col-sm-1 col-md-1 col-xl-0"></div>
@@ -22,12 +20,64 @@
 </template>
 
 <script>
-import Card from '../components/Card.vue'
+import Card from '../components/Card.vue';
+import axios from 'axios';
 export default {
     name: 'Trees',
     components: {
         Card
-    }
+    },
+
+    data() {
+        return {
+            userArray: [],
+            treeArray: [],
+            errors: []
+        }
+    },
+    /* https://randomuser.me/api/?results=10 */
+    // Fetches user from (https://jsonplaceholder.typicode.com/users) when the component is created.
+    created() {
+        axios.get('https://jsonplaceholder.typicode.com/users')
+            .then(response => {
+                // JSON responses are automatically parsed.
+                this.userArray = response.data
+                /* console.log(this.userArray); */
+            })
+            .then(
+                 this.treeArray = [
+            {
+                "id": 0,
+                "name": "Al",
+                "image": "../../assets/trees/al.png",
+                "description" : "hejsan 1"
+            },
+            {
+                "id": 1,
+                "name": "Alm",
+                "image": "../../assets/trees/alm.png",
+                "description" : "hejsan 2"
+            },
+            {
+                "id": 2,
+                "name": "Ask",
+                "image": "../../assets/trees/ask.png",
+                "description" : "hejsan 3"
+            },
+            {
+                "id": 3,
+                "name": "Asp",
+                "image": "../../assets/trees/asp.png",
+                "description" : "hejsan 4"
+            },
+        ]
+            ).catch(e => {
+                this.errors.push(e)
+            })
+
+
+    },
+
 }
 </script>
 
@@ -44,25 +94,25 @@ export default {
 
 /* Mobile */
 @media (min-width: 360px) and (max-width: 600px) {
-  #grid {
-    margin: 5%;
-    display: grid;
-    grid-template-columns: repeat(1, 1fr);
-    grid-template-rows: repeat(1, 1fr);
-    grid-column-gap: 20px;
-    grid-row-gap: 30px;
-}
+    #grid {
+        margin: 5%;
+        display: grid;
+        grid-template-columns: repeat(1, 1fr);
+        grid-template-rows: repeat(1, 1fr);
+        grid-column-gap: 20px;
+        grid-row-gap: 30px;
+    }
 }
 
 /* Tablet */
 @media (min-width: 768px) and (max-width: 1024px) {
-  #grid {
-    margin: 5%;
-    display: grid;
-    grid-template-columns: repeat(2, 1fr);
-    grid-template-rows: repeat(2, 1fr);
-    grid-column-gap: 20px;
-    grid-row-gap: 20px;
-}
+    #grid {
+        margin: 5%;
+        display: grid;
+        grid-template-columns: repeat(2, 1fr);
+        grid-template-rows: repeat(2, 1fr);
+        grid-column-gap: 20px;
+        grid-row-gap: 20px;
+    }
 }
 </style>
