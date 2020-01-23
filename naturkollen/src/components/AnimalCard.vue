db9e4522dd1ac9eb7263bc9f6e1a515f62d575fe
Jesper <jesper_truedsson@hotmail.com>
2020-01-23 18:56:15 +0100

    Fixed some code and new pic url

--- naturkollen/src/components/AnimalCard.vue
+++ naturkollen/src/components/AnimalCard.vue
@@ -1,73 +1,74 @@
 <template>
-<div>
+  <div>
     <div class="card">
-        <img :src="animal.image" class="img-fluid" />
+      <img :src="animal.image" class="img-fluid" />
 
-        <div class="card-body">
-            <h2 class="card-title" style="text-align:center;"> {{animal.name}}</h2>
-            <button class="btn btn-success" style="width:100%;" data-toggle="collapse" :data-target="'#collapseExample' + animal.id" aria-expanded="false" aria-controls="collapseExample" v-on:click="toggle">
-                Information
-            </button>
+      <div class="card-body">
+        <h2 class="card-title" style="text-align:center;">{{animal.name}}</h2>
+        <button
+          class="btn btn-success"
+          style="width:100%;"
+          data-toggle="collapse"
+          :data-target="'#collapseExample' + animal.id"
+          aria-expanded="false"
+          aria-controls="collapseExample"
+          v-on:click="toggle"
+        >Information</button>
 
-            <div class="collapse" :id="'collapseExample' + animal.id">
-                <div class="card-body">
-                    <p class="card-text">
-                        {{animal.description}}
-                    </p><br>
+        <div class="collapse" :id="'collapseExample' + animal.id">
+          <div class="card-body">
+            <p class="card-text">{{animal.description}}</p>
+            <br />
 
-                    <h5 class="card-title">{{animal.kidTitle}}</h5>
-                    <img :src="animal.kidImage" class="img-fluid" />
-                    <p>{{animal.kidDesc}}</p>
-                    
-                </div>
-            </div>
+            <h5 class="card-title">{{animal.kidTitle}}</h5>
+            <img :src="animal.kidImage" class="img-fluid" />
+            <p>{{animal.kidDesc}}</p>
+          </div>
         </div>
+      </div>
     </div>
-
-</div>
+  </div>
 </template>
 <script>
 export default {
-    name: "AnimalCard",
-    props: {
-        animal: {}
-    },
-
-    mounted: function () {
-
-    },
-
-    methods: {
-        toggle: function () {
-            let bodyID = this.animal.id;
-            console.log(bodyID);
-        }
+  name: "AnimalCard",
+  data() {
+    return {
+      bodyID: {}
+    };
+  },
+  props: {
+    animal: {}
+  },
+  methods: {
+    toggle: function() {
+      this.bodyID = this.animal.id;
     }
-}
+  }
+};
 </script>
 
 <style scoped>
 /* Desktop */
 .card {
-    border: 0;
-    width: 18rem; 
-    box-shadow: 1px 1px 10px #222;
+  border: 0;
+  width: 18rem;
+  box-shadow: 1px 1px 10px #222;
 }
 /* Mobile */
 @media (min-width: 360px) and (max-width: 600px) {
- .card {
+  .card {
     border: 0;
-    width: auto; 
+    width: auto;
     box-shadow: 1px 1px 10px #222;
-}
+  }
 }
 /* Tablet */
 @media (min-width: 768px) and (max-width: 1024px) {
-.card {
+  .card {
     border: 0;
-    width: auto; 
+    width: auto;
     box-shadow: 1px 1px 10px #222;
+  }
 }
-}
-
 </style>
--- naturkollen/src/components/Card.vue
+++ naturkollen/src/components/Card.vue
@@ -1,74 +1,72 @@
 <template>
-<div>
-
+  <div>
     <div class="card">
-        <!-- <img v-bind:src="tree.image" v-bind:alt="tree.image" class="img-fluid" /> -->
-        <img :src="tree.image" class="img-fluid" />
-             
-        <div class="card-body">
-            <h2 class="card-title" style="text-align:center;"> {{tree.name}}</h2>
-            <button class="btn btn-success" style="width:100%;"  data-toggle="collapse" :data-target="'#collapseExample' + tree.id" aria-expanded="false" aria-controls="collapseExample" v-on:click="toggle">
-                Information
-            </button>
+      <!-- <img v-bind:src="tree.image" v-bind:alt="tree.image" class="img-fluid" /> -->
+      <img :src="tree.image" class="img-fluid" />
 
-            <div class="collapse" :id="'collapseExample' + tree.id">
-                <div class="card-body">
-                    <p class="card-text">
-                        {{tree.description}}
-                    </p>
-                </div>
-            </div>
+      <div class="card-body">
+        <h2 class="card-title" style="text-align:center;">{{tree.name}}</h2>
+        <button
+          class="btn btn-success"
+          style="width:100%;"
+          data-toggle="collapse"
+          :data-target="'#collapseExample' + tree.id"
+          aria-expanded="false"
+          aria-controls="collapseExample"
+          v-on:click="toggle"
+        >Information</button>
+
+        <div class="collapse" :id="'collapseExample' + tree.id">
+          <div class="card-body">
+            <p class="card-text">{{tree.description}}</p>
+          </div>
         </div>
+      </div>
     </div>
-
-
-</div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "Card",
-    props: {
-        /* user: {}, */
-        animal: {},
-        tree: {}
-    },
-
-    mounted: function () {
-
-    },
-
-    methods: {
-        toggle: function () {
-            let bodyID = this.tree.id;
-            console.log(bodyID);
-        }
+  name: "Card",
+  data(){
+      return{
+          bodyID: {}
+      }
+  },
+  props: {
+    animal: {},
+    tree: {}
+  },
+  methods: {
+    toggle: function() {
+      this.bodyID = this.tree.id;
     }
-}
+  }
+};
 </script>
 
 <style scoped>
 /* Desktop */
 .card {
-    border: 0;
-    width: 18rem; 
-    box-shadow: 1px 1px 10px #222;
+  border: 0;
+  width: 18rem;
+  box-shadow: 1px 1px 10px #222;
 }
 /* Mobile */
 @media (min-width: 360px) and (max-width: 600px) {
- .card {
+  .card {
     border: 0;
-    width: auto; 
+    width: auto;
     box-shadow: 1px 1px 10px #222;
-}
+  }
 }
 /* Tablet */
 @media (min-width: 768px) and (max-width: 1024px) {
-.card {
+  .card {
     border: 0;
-    width: auto; 
+    width: auto;
     box-shadow: 1px 1px 10px #222;
+  }
 }
-}
-
 </style>
